3d67209c1f5c284ef779cfa1b3ddf47af561c522
Farhan Masood <farhan.masood@mslm.io>
2024-01-26 09:54:01 +0500

    discord clone starter

---
+++ app/(main)/(routes)/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/app/(main)/components/ui/button";
+
+export default function Home() {
+  return (
+    <div className="">
+      <div className="text-3xl text-red-50">Hyy</div>
+      <Button className="ml-10">Hy</Button>
+    </div>
+  );
+}
